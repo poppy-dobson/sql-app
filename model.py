0dc180b51a42aefc8195c9a237ac513122740aa3
poppy-dobson <poppy.dobson138@gmail.com>
2026-01-19 18:30:34 +0000

    initial setup of model.py file (very very incomplete)

--- model.py
+++ model.py
@@ -1,4 +1,17 @@
-# from langchain_huggingface ??? import ...
+#import requests
+import os
+import tomllib
 from pydantic import BaseModel, Field
-
+from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv
 # ...
+
+load_dotenv()
+
+class ModelQuizQuestionOutput(BaseModel):
+  # TO BE MODIFIED LATER, this is just place-holding for now
+  question: str
+  correct_answer: str
+
+
+quiz_model = HuggingFaceEndpoint() # will be added to
