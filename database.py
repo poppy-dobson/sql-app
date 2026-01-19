e64b5337794911dde31668a96c3f34622f1d7afd
poppy-dobson <poppy.dobson138@gmail.com>
2026-01-19 21:45:29 +0000

    initial database model work

--- database.py
+++ database.py
@@ -1,15 +1,41 @@
 import sqlalchemy as sqla
+from sqlalchemy import create_engine
 # import sqlite3
 
 # ...
 
-def check_valid_db_file(db_file):
+def check_valid_db_file(db_bytes):
   '''
   args:
-  db_file: BytesIO of an sqlite database file
+  db_bytes: BytesIO of an sqlite database file
 
   return:
   bool: if the database input is valid / can be opened by the program
   '''
   # for now:
   return True
+
+def sqlite_engine_string(db_path):
+  return "sqlite:///" + db_path
+
+# very pseudocod-y implementation, for now
+class DataBaseInteraction:
+  def __init__(self, engine_string:str):
+    self.engine = create_engine(engine_string, connect_args={"autocommit": False})
+    self.schema = self._set_schema()
+    pass
+
+  def _set_schema(self):
+    # execute query to get the schema
+    # return schema
+    pass
+
+  def get_schema(self):
+    # return self.schema (text of SQL schema statements (create table, etc))
+    pass
+
+  def execute_query(self, query):
+    with self.engine.connect as conn:
+      pass # ???
+    # return result
+    pass
