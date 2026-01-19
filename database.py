2d80166052620c06a13bfabe80767eea8daa93cd
poppy-dobson <poppy.dobson138@gmail.com>
2026-01-19 18:05:05 +0000

    added checking sqlite db functionality to this module rather than in app.py

--- database.py
+++ database.py
@@ -2,3 +2,14 @@ import sqlalchemy as sqla
 # import sqlite3
 
 # ...
+
+def check_valid_db_file(db_file):
+  '''
+  args:
+  db_file: BytesIO of an sqlite database file
+
+  return:
+  bool: if the database input is valid / can be opened by the program
+  '''
+  # for now:
+  return True
