1f2588b153ad67ed3ce67bdd4f611a142418777b
poppy-dobson <poppy.dobson138@gmail.com>
2026-01-25 20:58:10 +0000

    working quiz implementation!

--- pages/quiz.py
+++ pages/quiz.py
@@ -18,12 +18,19 @@ class QuizElement:
     self.answerable = True
     self.correct = None
 
+  def show(self):
     st.write("Question " + self.key + ":")
     st.write(self.question)
-    self.open_to_response()
+    if self.answerable:
+      self.open_to_response()
+    else:
+      st.write("Your answer:")
+      try: st.write(self.user_answer)
+      except: pass # lazy error handling - will fix
 
     # for debugging
-    st.write(self.model_answer)
+    # st.write(self.model_answer)
+    #
 
     st.divider()
 
@@ -39,40 +46,35 @@ class QuizElement:
   def set_correct(self, is_correct):
     self.correct = is_correct
 
-st.title("the quiz!")
 
-# here, before the form, will display the user's schema from the database object!
-st.write("The database schema:")
-for table in st.session_state.database.get_schema():
-  if not table[0]: continue
-  table_formatted = table[0] + ';'
-  st.code(table_formatted, wrap_lines=True)
+def execute_answer_query(query):
+  try:
+    result = st.session_state.database.execute_query(query)
+    return result
+  except:
+    print("query failed to execute") # i'm debugging
+    #raise RuntimeError
+    return []
 
-def mark_question(model_answer, user_answer):
-  if model_answer == user_answer:
-    return True
-  else:
-    model_answer_result = st.session_state.database.execute_query(model_answer)
-    try:
-      user_answer_result = st.session_state.database.execute_query(user_answer)
-    except Exception as e: # make this more specific and return a specific error message
-      print("the user query failed to execute") # temp error handling
-      return False
-  
-  if model_answer_result == user_answer_result:
-    return True
-  return False
-
-def _quiz_submitted():
+def quiz_submitted():
   score = 0
   incorrect_questions = []
-  for element in st.session_state.quiz_question_form_elements:
-    user_answer = element.get_user_answer()
-    element.lock()
-    st.session_state.user_answers.append(user_answer)
+
+  for i, element in enumerate(st.session_state.quiz_question_form_elements):
+    st.write(f"Question {i+1}:")
+
+    model_answer_result = execute_answer_query(element.model_answer)
+    user_answer_result = execute_answer_query(element.user_answer)
+
+    st.write("The result of your query:")
+    st.dataframe(data=user_answer_result)
+
+    st.write("The correct result:")
+    st.write(element.model_answer)
+    st.dataframe(model_answer_result)
 
     # mark each question as right/wrong
-    correct = mark_question(element.model_answer, element.get_user_answer())
+    correct = (element.model_answer.upper() == element.user_answer.upper()) or (model_answer_result == user_answer_result)
 
     print(f"question is {correct}")
 
@@ -93,6 +95,7 @@ def _quiz_submitted():
   else:
     st.write("You got every question right! Well Done!")
 
+
 def get_feedback_on_incorrect_answers(incorrect_questions):
   prompt_input = ""
   for element in incorrect_questions:
@@ -108,54 +111,73 @@ User Answer (incorrect): {element.get_user_answer()}
   feedback = st.session_state.model.get_quiz_answer_feedback(prompt_input)
   return feedback
 
-def show_results():
-  # not implemented yet
-  pass
-
-# shoddy debugging
-print(st.session_state.quiz)
-print(st.session_state.submitted)
-
-if (not st.session_state.quiz) and (not st.session_state.submitted):
-  with st.spinner("generating quiz..."):
-
-    print("generating quiz")
-
-    try:
-      quiz = st.session_state.model.generate_quiz(st.session_state.topics)
-      st.session_state.quiz = quiz
-
-      print("quiz made")
-      print(st.session_state.quiz)
-
-      try:
-        assert len(quiz) == config['quiz']['num_questions']
-      except:
-        print("quiz is not the right length")
-    except:
-      # temporary lazy error handling
-      print("quiz failed to generate")
+def all_answers_have_been_entered():
+  for element in st.session_state.quiz_question_form_elements:
+    if not element.user_answer:
+      return False
+  return True
 
 def submit_pressed():
-  st.session_state.submitted = True
+  if all_answers_have_been_entered():
+    for element in st.session_state.quiz_question_form_elements:
+      element.lock()
+    
+    st.session_state.submitted = True
+  else:
+    st.toast("you must enter something for every question")
 
-if not st.session_state.submitted:
+
+def display_quiz():
   with st.form("quiz1") as quiz_form:
-    print("form generated") # debugging
-    for i, question_and_answer in enumerate(st.session_state.quiz):
-      st.session_state.quiz_question_form_elements.append(QuizElement(str(i+1), question_and_answer))
+    for element in st.session_state.quiz_question_form_elements:
+      element.show()
 
     #submitted = st.form_submit_button("Submit Answers")
     st.form_submit_button("Submit Answers", on_click=submit_pressed)
 
-if st.session_state.submitted:
+def generate_quiz_questions():
+  with st.spinner("generating quiz..."):
 
-  print("button pressed, quiz submitted")
-  print(st.session_state.submitted)
+      try:
+        quiz = st.session_state.model.generate_quiz(st.session_state.topics)
+        st.session_state.quiz = quiz
 
-  st.toast("u submitted the quiz!")
-  #st.session_state.submitted = True
-  _quiz_submitted()
+        print("quiz made")
+        print(st.session_state.quiz)
+
+        for i, question_and_answer in enumerate(st.session_state.quiz):
+          st.session_state.quiz_question_form_elements.append(QuizElement(str(i+1), question_and_answer))
+
+        try:
+          assert len(quiz) == config['quiz']['num_questions']
+        except:
+          print("quiz is not the right length")
+      except:
+        # temporary lazy error handling
+        print("quiz failed to generate")
 
 
+def main():
+  st.title("the quiz!")
+
+  st.write("The database schema:")
+  for table in st.session_state.database.get_schema():
+    if not table[0]: continue
+    table_formatted = table[0] + ';'
+    st.code(table_formatted, wrap_lines=True)
+
+  if (not st.session_state.quiz) and (not st.session_state.submitted):
+    generate_quiz_questions()
+
+  if not st.session_state.submitted:
+    display_quiz()
   
+  if st.session_state.submitted:
+    print("button pressed, quiz submitted")
+
+    st.toast("u submitted the quiz!")
+    #st.session_state.submitted = True
+    display_quiz() # should have element.answerable = False
+    quiz_submitted()
+  
+main()
